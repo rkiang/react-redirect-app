c94674a6138b933df4477767db9996d7740ded8c
rkiang <ryanlkiang@gmail.com>
2018-04-06 13:30:22 -0500

    added get request for user info to dashboard

--- routes/api.js
+++ routes/api.js
@@ -86,4 +86,23 @@ router.post('/new-user', function (req, res) {
     console.log(req.body);
 });
 
+// Handles Ajax request for user information if user is authenticated
+router.get('/dashboard', function (req, res) {
+    console.log('get /user route');
+    // check if logged in
+    if (req.isAuthenticated()) {
+        // send back user object from database
+        console.log('logged in', req.user);
+        var userInfo = {
+            username: req.user.username
+        };
+        res.send(userInfo);
+    } else {
+        // failure best handled on the server. do redirect here.
+        console.log('not logged in');
+        // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+        res.send(false);
+    }
+});
+
 module.exports = router;
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Dashboard = () => (
-    <div>
-        <h2>Welcome</h2>
-    </div>
-);
+const url = 'http://localhost:5000/api';
+
+class Dashboard extends Component {
+    constructor() {
+        super();
+        this.state = {
+          username: '',
+        }
+      }
+
+
+
+    userInfo() {
+        fetch(`${url}/dashboard`)
+            .then(response => response.json())
+            //     if (response.userInfo) {
+            //         const { userInfo } = this.username;
+            //         console.log('userInfo: ', this.userInfo);
+            //     }
+            // })
+            .then(userInfo => {
+                console.log(userInfo);
+                this.setState({
+                    users: userInfo
+                });
+            })
+            .catch(error => console.log(`Error with Fetch getUsers: ${error}`));
+    }
+
+    componentDidMount() {
+        console.log('component has mounted');
+        this.userInfo();
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Welcome</h2>
+            </div>
+        );
+    }
+}
+
 
 export default Dashboard;
